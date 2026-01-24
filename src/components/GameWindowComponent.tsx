56f79a15b6677490390c8644afe8e87b650f5c09
peletic <its.peletic@gmail.com>
2026-01-24 15:22:11 -0800

    adds game window and game entrypoint

---
+++ src/components/GameWindowComponent.tsx
@@ -0,0 +1,17 @@
+"use client"
+import {webEngine} from "sprig/web"
+import {useEffect} from "react";
+import {start} from "../game";
+import clsx from "clsx";
+
+export default function GameWindowComponent({width, className = ""}: { width: number, className?: string}) {
+    useEffect(() => {
+        const engine = webEngine(document.getElementById("canvas") as HTMLCanvasElement)
+        start(engine.api)
+    }, []);
+
+    return (<>
+        <canvas className={clsx("border-2 w-fit", className)} width={width} height={(width * 4) / 5} id="canvas" tabIndex={0}></canvas>
+    </>)
+
+}
