d3db241ab3d4e5c71002d94b6da5daa00cffe67a
Peletic <its.peletic@gmail.com>
2026-01-29 19:58:40 -0800

    allow for better import paths

--- app/src/components/GameWindowComponent.tsx
+++ app/src/components/GameWindowComponent.tsx
@@ -1,7 +1,7 @@
 "use client"
 import {webEngine} from "sprig/web"
 import {useEffect} from "react";
-import {start} from "../../../game";
+import {start} from "game/index";
 import clsx from "clsx";
 
 export default function GameWindowComponent({width, className = ""}: { width: number, className?: string}) {
