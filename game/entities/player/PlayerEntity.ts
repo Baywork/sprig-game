81603c22dfce1476748fa568132d491b0eb50ddb
peletic <its.peletic@gmail.com>
2026-02-01 17:31:12 -0800

    movement

--- game/entities/player/PlayerEntity.ts
+++ game/entities/player/PlayerEntity.ts
@@ -1,32 +1,63 @@
 import Entity from "game/entities/Entity.ts";
-import type { GameState } from "game/GameState";
+import type {GameState} from "game/GameState";
 import Screen from "game/graphics/Screen"
 
 export default class PlayerEntity extends Entity {
     x: number
     y: number
 
+    xVelo: number;
+    yVelo: number
+
     constructor() {
         super();
         this.x = 0;
         this.y = 0;
+
+        this.xVelo = 0;
+        this.yVelo = 0;
     }
 
 
     next(gameState: GameState): void {
+        if (Math.abs(this.xVelo) > 0) {
+            this.xVelo = Math.floor(this.xVelo/2)
+        }
+        if (Math.abs(this.yVelo) > 0) {
+            this.yVelo = Math.floor(this.yVelo/2)
+        }
 
-        this.x += 1;
-        this.y += 1;
+        if (this.xVelo == 0 && this.yVelo == 0) {
+            if (gameState.wHeld) {
+                this.yVelo -= dashVeloAdd
+            }
+            if (gameState.sHeld) {
+                this.yVelo += dashVeloAdd
+            }
+            if (gameState.aHeld) {
+                this.xVelo -= dashVeloAdd
+            }
+            if (gameState.dHeld) {
+                this.xVelo += dashVeloAdd
+            }
+        }
+
+        this.x += this.xVelo;
+        this.y += this.yVelo;
+
     }
+
     draw(screen: Screen): Screen {
         screen.setPixelsAt(this.x, this.y, this.toString())
         return screen
     }
+
     toString(): string {
         return placeholder
     }
 }
 
+const dashVeloAdd = 5
 const placeholder = `..00.........0..
 ..00........00..
 ..00.......000..
