5c766bc17b4a7edbd9efc58e2b1cf69818d27834
Peletic <its.peletic@gmail.com>
2026-02-10 22:41:46 -0800

    improve collisions

--- game/entities/Entity.ts
+++ game/entities/Entity.ts
@@ -37,13 +37,27 @@ export default abstract class Entity extends Body {
             let potentialY = Math.round((this.yVelocity - this.gravityVelocity) * deltaTime / 100) + this.y
 
             //console.log((this.xVelocity * deltaTime)/1000)
-            const xCollisions = this.game.world.getCollidingBodies(potentialX, this.y, potentialX + this.width, this.y + this.height).flat(2).filter((val) => val != this)
-            const yCollisions = this.game.world.getCollidingBodies(this.x, potentialY, this.x + this.width, potentialY + this.height).flat(2).filter((val) => val != this)
+            const xCollisions: Body[] = this.game.world.getCollidingBodies(potentialX, this.y, potentialX + this.width, this.y + this.height).flat(2).filter((val) => val != this)
+            const yCollisions: Body[] = this.game.world.getCollidingBodies(this.x, potentialY, this.x + this.width, potentialY + this.height).flat(2).filter((val) => val != this)
 
             if (potentialY !== this.y) console.log(potentialY)
-            // todo: actually complete collision code. this only checks to see if any body potentially collides and prevents that rather than moving to the maximum possible non collision
-            if (xCollisions.length == 0) this.x = potentialX
-            if (yCollisions.length == 0) this.y = potentialY
+
+            for (const collision of xCollisions) {
+                // -1:L 1:R
+
+                if (potentialX < collision.x && collision.x + collision.width > potentialX) potentialX = collision.x + collision.width + 1
+                else if (potentialX > collision.x) potentialX = collision.x - this.width - 1
+            }
+
+            for (const collision of yCollisions) {
+                if (collision.y < potentialY && collision.y + collision.height > potentialY) {
+                    console.log("y col")
+                    potentialY = collision.y + collision.height
+                }
+                else if (collision.y > potentialY) potentialY = collision.y - this.height - 1
+            }
+            this.x = potentialX
+            this.y = potentialY
         }
     }
 
