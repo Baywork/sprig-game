abf64edd35e8b9d68c4aa4aa16f75c48073e744d
jackrevson <jack.revson@fusionacademy.me>
2026-02-11 13:22:27 -0800

    Increase gravity velocity calculation factor

--- game/physics/physics.ts
+++ game/physics/physics.ts
@@ -8,6 +8,6 @@ const GRAVITY = 10;
 export function gravityVelocity(previousGravity: number, dTimeMS: number): number {
     if (previousGravity >= TERMINAL_VELOCITY) return TERMINAL_VELOCITY
 
-    const addtVelocity = GRAVITY * dTimeMS * (1 / 400)
+    const addtVelocity = GRAVITY * dTimeMS * (1 / 100)
     return previousGravity + addtVelocity
 }
