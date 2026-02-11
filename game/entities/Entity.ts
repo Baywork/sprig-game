bef34f8d2d5a49d16710eecde183c56efa7a2303
Peletic <its.peletic@gmail.com>
2026-02-10 20:14:52 -0800

    fix some collision stuff

--- game/entities/Entity.ts
+++ game/entities/Entity.ts
@@ -7,8 +7,7 @@ export default abstract class Entity extends Body {
     yVelocity: number
     gravityVelocity: number
     isOnGround: () => boolean = () => {
-        const colliders = this.game.world.getBodiesCollidingWith(this.expand(4))
-
+        const colliders = this.game.world.getBodiesCollidingWith(this.expand(1))
         for (const body of colliders) {
             if (body.y <= this.y) return true
         }
@@ -37,14 +36,13 @@ export default abstract class Entity extends Body {
             let potentialY = Math.round((this.yVelocity - this.gravityVelocity) * deltaTime / 100) + this.y
 
             //console.log((this.xVelocity * deltaTime)/1000)
-            const xCollisions = this.game.world.getRangeBodies(potentialX, this.y, potentialX + this.width, this.y + this.height)
-            const yCollisions = this.game.world.getRangeBodies(this.x, potentialY, this.x + this.width, potentialY + this.height)
+            const xCollisions = this.game.world.getCollidingBodies(potentialX, this.y, potentialX + this.width, this.y + this.height).flat(2).filter((val) => val != this)
+            const yCollisions = this.game.world.getCollidingBodies(this.x, potentialY, this.x + this.width, potentialY + this.height).flat(2).filter((val) => val != this)
 
+            if (potentialY !== this.y) console.log(potentialY)
             // todo: actually complete collision code. this only checks to see if any body potentially collides and prevents that rather than moving to the maximum possible non collision
-            /*if (xCollisions.length == 0)*/
-            this.x = potentialX
-            /*if (yCollisions.length == 0)*/
-            this.y = potentialY
+            if (xCollisions.length == 0) this.x = potentialX
+            if (yCollisions.length == 0) this.y = potentialY
         }
     }
 
--- game/world/World.ts
+++ game/world/World.ts
@@ -8,6 +8,7 @@ export default class World {
     mapHeight: number
     entities: Entity[]
 
+    bufferSize: number = 16
     distanceUnitResolution: number = 12
 
     // maybe we manage our max width and height somewhere else? dunno for now
@@ -42,15 +43,27 @@ export default class World {
         }
         for (const entity of this.entities) {
             // todo: this is NOT a fix.
-            if (entity.x > minX && entity.y > minY) {
-                if (entity.isWithinRange(minX, minY, maxX, maxY)) res[entity.x - minX][entity.y - minY].push(entity)
+            try {
+
+                if (entity.isWithinRange(minX, minY, maxX, maxY)) {
+                    if (entity.x < minX || entity.y < minY) {
+                        console.log("range resolution overlapped with out of bounds entity")
+
+                    } else {
+                        res[entity.x - minX][entity.y - minY].push(entity)
+                    }
+                }
+            } catch (e) {
+                console.error(e)
+                console.log(`${entity.x} - ${minX}`)
+                console.log(`${entity.y} - ${minY}`)
             }
         }
 
         return res
     }
 
-    getBodiesCollidingWith(body: Body, searchRadius: number = 4) {
+    getBodiesCollidingWith(body: Body, searchRadius: number = this.bufferSize) {
         const xA = body.x;
         const yA = body.y;
         const xB = body.x + body.width;
@@ -61,7 +74,7 @@ export default class World {
     }
 
 
-    getCollidingBodies(xA, yA, xB, yB, searchRadius: number = 4) {
+    getCollidingBodies(xA: number, yA: number, xB: number, yB: number, searchRadius: number = this.bufferSize) {
         const res = []
         let lowerX = xA - searchRadius
         let lowerY = yA - searchRadius
@@ -85,9 +98,13 @@ export default class World {
 
     addBody(body: Body) {
         this.map[body.x][body.y].push(body)
+        if (body.width > this.bufferSize) this.bufferSize = body.width
+        if (body.height > this.bufferSize) this.bufferSize = body.height
     }
 
     addEntity(entity: Entity) {
         this.entities.push(entity)
+        if (entity.width > this.bufferSize) this.bufferSize = entity.width
+        if (entity.height > this.bufferSize) this.bufferSize = entity.height
     }
 }
