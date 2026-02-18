0206bac8a9e684cd2fe39fbbfc34dd2408724533
peletic <its.peletic@gmail.com>
2026-02-18 13:14:23 -0800

    implement slightly better map instantiation

--- game/map/transformer/dictionary.ts
+++ game/map/transformer/dictionary.ts
@@ -3,8 +3,13 @@ import {Body} from "game/body/Body.ts";
 import {EarthBody} from "game/body/platform/EarthBody.ts";
 import {CloudBody} from "game/body/platform/CloudBody.ts";
 
-export const tileHexMap: {[p: string] : (...args : ConstructorParameters<typeof Body>) => Body} = {
-    "26dc36": (...args: ConstructorParameters<typeof Body>): Body => new GrassBody(...args),
-    "a59041": (...args: ConstructorParameters<typeof Body>): Body => new EarthBody(...args),
-    "e2c1ff": (...args: ConstructorParameters<typeof Body>): Body => new CloudBody(...args)
+const hexReferenceTable = {
+    "26dc36": GrassBody,
+    "a59041": EarthBody,
+    "e2c1ff": CloudBody
+}
+
+
+export const tileHexMap : {[p: string] : (...args : ConstructorParameters<typeof Body>) => Body} = {
+    ...Object.fromEntries(Object.entries(hexReferenceTable).map(([key, BodyType])=> [key, (...args: ConstructorParameters<typeof Body>): Body => new BodyType(...args)]))
 }
