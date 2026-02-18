913ac1edf824937063e7866fcc25a530cb6b8254
peletic <its.peletic@gmail.com>
2026-02-18 13:16:18 -0800

    remove old boilerplate in preparation for parallel repo integration

--- game/ui/menus/menu.ts
+++
@@ -1,8 +0,0 @@
-import type {InputKey} from "sprig";
-import type {GameState} from "game/GameState";
-
-export abstract class Menu {
-    abstract onInput(key: InputKey, gameState: GameState): GameState
-
-    abstract nextFrame(): string
-}
