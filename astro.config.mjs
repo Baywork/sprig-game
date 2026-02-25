20d6fe36f11c70df20f7c52869da055d8e6357b8
peletic <its.peletic@gmail.com>
2026-02-25 12:37:12 -0800

    begin touching on no-preflight asset handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-
+import * as fs from "node:fs"
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(),
+      {
+        name: "raw-asset-loader",
+        transform(src, id) {
+          if (id.endsWith('.png') && !id.includes('?raw')) {
+            const content = fs.readFileSync(id, 'utf-8');
+            return `export default ${JSON.stringify(content)}`;
+          }
+        }
+      }]
   },
   root: "./app",
   srcDir: "./app/src",
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("node:path");
 
 module.exports = {
     entry: "./game/index.ts",
+    mode: "production",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
@@ -15,14 +16,19 @@ module.exports = {
                 use: "ts-loader",
                 exclude: /node_modules/,
             },
+            {
+                test: /\.png/,
+                type: 'asset/bytes',
+            },
         ],
     },
     resolve: {
         alias: {
             "game": path.resolve(__dirname, "game"),
             "app": path.resolve(__dirname, "app/src"),
-            "lib": path.resolve(__dirname, "lib")
+            "lib": path.resolve(__dirname, "lib"),
+            "assets": path.resolve(__dirname, "assets")
         },
-        extensions: ['.ts', '.js', '.json']
+        extensions: ['.ts', '.js', '.json', '.png']
     }
 };
